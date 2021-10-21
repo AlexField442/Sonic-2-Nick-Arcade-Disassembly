24866a37b1a51a6e745ec061be7aaeb163071d53
RepellantMold <repellantmold@outlook.com>
2021-10-21 07:01:50 -0700

    ripping palettes

--- Macros.asm
+++ Macros.asm
@@ -1,13 +1,11 @@
-align macro
+align macros
 	dcb.b (\1-(*%\1))%\1,0
-	endm
 
 stopZ80 macro
 	move.w	#$100,(Z80_Bus).l
-@loop:	btst	#0,(Z80_Bus).l
-	bne.s	@loop
+.loop:	btst	#0,(Z80_Bus).l
+	bne.s	.loop
 	endm
 
-startZ80 macro
+startZ80 macros
 	move.w	#0,(Z80_Bus).l
-	endm
--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
 IF EXIST s2built.bin move /Y s2built.bin s2built.prev.bin >NUL
-asm68k /k /p /o ae-,c+ s2.asm, s2built.bin, , s2.lst >s2.log
+asm68k /k /p /o ae-,c+,l+ s2.asm, s2built.bin, , s2.lst >s2.log
